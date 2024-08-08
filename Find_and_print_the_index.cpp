6a11cb67b8817361a05235db5108691326b10487
Hussain Tayyebi <141639385+Hussain1402@users.noreply.github.com>
2024-08-08 11:56:07 +0530

    Create Find_and_print_the_index.cpp

---
+++ Find_and_print_the_index.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x[5],key=26,a=0;
+    cout<<"Enter the elements in the array:"<<endl;
+    for(int i=0;i<5;i++){
+        cin>>x[i];
+    }
+    for (int i=0;i<5;i++){
+        if (key==x[i]){
+            a+=1;
+            cout<<"Key is present at "<<i<<".";
+            break;
+        }
+    }
+    if(a==0){
+        cout<<"Key is not present in the array.";
+    }
+    return 0;
+}
+/*
+Enter the elements in the array:
+56
+89
+342
+26
+45
+Key is present at 3.
+
+Enter the elements in the array:
+56
+76
+34
+89
+67
+Key is not present in the array.
+*/
