909fbcf004341f9e0079cdeffd5dd9edd0a6ead3
Hussain Tayyebi <141639385+Hussain1402@users.noreply.github.com>
2024-08-08 11:15:29 +0530

    Create Display_array.cpp

---
+++ Display_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x[6]={10,21,32,43,56,7};
+    for (int i=0;i<6;i++){
+        cout<<x[i]<<endl;
+    }
+    return 0;
+}
+/*
+10
+21
+32
+43
+56
+7
+*/
