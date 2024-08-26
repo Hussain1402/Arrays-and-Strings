cc07c716be484db9bf9f1816aaa7002f5e467442
Hussain Tayyebi <141639385+Hussain1402@users.noreply.github.com>
2024-08-26 22:19:34 +0530

    Create Concatenation_of_strings.cpp

---
+++ Concatenation_of_strings.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Hussain";
+    last_name = "Tayyebi";
+
+    cout<<first_name+last_name;
+}
+
+/*
+HussainTayyebi
+*/
