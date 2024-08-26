ee9eda9dc0463626d0aeeebe5fcbfc71dcdb1113
Hussain Tayyebi <141639385+Hussain1402@users.noreply.github.com>
2024-08-26 22:18:17 +0530

    Create Display_string.cpp

---
+++ Display_string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str1[4] = "C++";
+    char str2[] = {'h','+','+','\0'};
+    string str3 = "C++";
+
+    cout<<str1<<endl<<str2<<endl<<str3;
+}
+
+/*
+C++
+h++
+C++
+*/
